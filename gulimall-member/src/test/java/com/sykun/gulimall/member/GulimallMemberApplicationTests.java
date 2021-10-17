11c517e13155f43768d49dcc3b1d7dacf5c63511
SYK <sykun321@163.com>
2021-10-17 19:50:45 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/sykun/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sykun.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
